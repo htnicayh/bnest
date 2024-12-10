db87cb1d6164d7e07385213275ab6eb62738966f
hyacinthz <hieuhpcf@gmail.com>
2024-12-10 15:44:25 +0700

    chore: graphql

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { FastifyAdapter } from '@nestjs/platform-fastify'
 import 'reflect-metadata'
 import { AppModule } from './modules/app.module'
 
 async function bootstrap() {
 	const APPLICATION = 'NestApplication'
 	const logger = new Logger(APPLICATION)
-	const app = await NestFactory.create(AppModule)
+	const app = await NestFactory.create(AppModule, new FastifyAdapter())
 
-	await app.listen(3000)
+	await app.listen(process.env.PORT || 3000)
 
 	logger.log(
 		`Application is running on: \x1b[31m${`${await app.getUrl()}`}\x1b[0m`
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common'
+import { AppResolver } from '../resolvers/app.resolver'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 
-@Module({})
+@Module({
+	imports: [
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			autoSchemaFile: true
+		})
+	],
+	providers: [AppResolver]
+})
 export class AppModule {}
