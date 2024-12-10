1edcf74261331db895f297c00bad1d52c71c8344
hyacinthz <hieuhpcf@gmail.com>
2024-12-10 15:31:28 +0700

    chore: rest

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common'
+import { AppController } from '../controllers/app.controller'
+import { AppService } from '../services/app.service'
 
-@Module({})
+@Module({
+	controllers: [AppController],
+	providers: [AppService]
+})
 export class AppModule {}
